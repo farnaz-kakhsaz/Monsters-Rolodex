a4008ecaf40f3fd93ca573c9212fe056c141ac14
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2020-06-15 23:01:07 +0430

    Updates

---
+++ precache-manifest.44ea2076730b85f62191d74c5a292918.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c0cd7e7fb430d4afbaa40e7c93bd02",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "c0bc345a782a20ffb670",
+    "url": "/Monsters-Rolodex/static/css/main.b938fae7.chunk.css"
+  },
+  {
+    "revision": "8eefb47c5e7c6f1166f6",
+    "url": "/Monsters-Rolodex/static/js/2.27033893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.27033893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0bc345a782a20ffb670",
+    "url": "/Monsters-Rolodex/static/js/main.ed9d97ba.chunk.js"
+  },
+  {
+    "revision": "bddd2872dd5650c52730",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.dc822ace.js"
+  }
+]);
